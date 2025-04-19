3511f19dd4ff8dd05902b31863baffea887a6744
Pham Thanh Tung <46311615+ttpham38@users.noreply.github.com>
2025-04-19 18:49:31 +0700

    Add files via upload

---
+++ xulyanhso.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Xử lý ảnh số",
+    page_icon="👋",
+)
+st.write("# Chào mừng bạn đến với Project xử lý ảnh số👋")
+st.write("## Tôi là Phạm Thanh Tùng - 23610012👋")
+
+st.sidebar.success("Hãy chọn menu trên")
+
